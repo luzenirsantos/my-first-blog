d0fa1efdd82e9987164e24440982e36e8f8b051d
luzenirsantos <lusantos.lps@gmail.com>
2019-01-24 11:29:00 -0300

    Changed the HTML for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
+admin.site.site_header = 'Painel de Controle'
+admin.site.index_title = 'Recursos'
+admin.site.site_title = 'Blog da Lu'
